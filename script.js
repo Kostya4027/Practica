d6e3117bebbf9bf4391f456e4ece5a226f6cb593
Kostya4027 <153510541+Kostya4027@users.noreply.github.com>
2024-05-20 09:15:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+new Swiper('.image-slider', {
+    slidesPerGroup: 3,
+    spaceBetween: 0,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+});
